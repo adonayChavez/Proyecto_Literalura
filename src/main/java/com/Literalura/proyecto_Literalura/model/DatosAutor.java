9474622a15b920b1119c875435127ff2886219ae
AdonayChavez <chavezadonay3@gmail.com>
2024-12-04 21:12:14 -0600

    modelos, mapeo de datos a utilizar

---
+++ src/main/java/com/Literalura/proyecto_Literalura/model/DatosAutor.java
@@ -0,0 +1,10 @@
+package com.Literalura.proyecto_Literalura.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosAutor(
+        @JsonAlias("name") String nombre,
+        @JsonAlias("birth_year") int fechaDeNacimiento) {
+}
